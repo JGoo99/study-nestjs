d4b0e2d52fabdf781f3b7dfc1000750274d6a4bd
JinGoo <zzgg3040@naver.com>
2025-03-10 01:34:49 +0900

    first-commit

---
+++ letsStart/dist/app.js
@@ -0,0 +1,36 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var app_model_1 = require("./app.model");
+var app = express();
+app.use(function (req, res, next) {
+    console.log(req.rawHeaders[1]);
+    console.log("this is logging middleware");
+    next();
+});
+app.get("/cats", function (req, res) {
+    try {
+        var cats = app_model_1.Cat;
+        throw new Error("db connect error");
+        res.status(200).send({
+            success: true,
+            data: {
+                cats: cats,
+            },
+        });
+    }
+    catch (error) {
+        res.status(400).send({
+            success: false,
+            error: error.message,
+        });
+    }
+});
+app.use(function (req, res, next) {
+    console.log("this is error middleware");
+    console.log({ error: "404 not found error" });
+});
+app.listen(8000, function () {
+    console.log("server is on...");
+});
+//# sourceMappingURL=app.js.map
---
+++ letsStart/src/app.ts
@@ -0,0 +1,42 @@
+import * as express from "express";
+import { Cat } from "./app.model";
+
+const app: express.Express = express();
+
+// logging middleware
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]); // 요청 위치
+  console.log("this is logging middleware");
+  next();
+});
+
+// READ 고양이 전체 데이터 조회
+app.get("/cats", (req, res) => {
+  try {
+    const cats = Cat;
+    throw new Error("db connect error");
+    res.status(200).send({
+      success: true,
+      data: {
+        cats,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+// READ 특정 고양이 데이터 조회
+
+// 404 middleware
+app.use((req, res, next) => {
+  console.log("this is error middleware");
+  console.log({ error: "404 not found error" });
+});
+
+app.listen(8000, () => {
+  console.log("server is on...");
+});
